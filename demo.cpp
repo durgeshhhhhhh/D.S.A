02ac79c35b7ad47109727252e2490282e7145251
Durgesh shekhawat <durgeshshekhawat123456@gmail.com>
2024-12-14 13:48:19 +0530

    Implement recursive function to reverse an array in C++.

--- demo.cpp
+++ demo.cpp
@@ -1,27 +1,26 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-void print(int n, int i, int r,  int arr[])
+void print(int n, int i, int arr[])
 {
-  if (i >= r)
+  if (i >= n/2)
     return;
 
-  swap(arr[i], arr[r]);
+  swap(arr[i], arr[n-i-1]);
 
-  print(n, i + 1, r-1, arr);
+  print(n, i + 1, arr);
 }
 int main()
 {
   int n;
   cin >> n;
   int arr[n];
-  int r = n-1;
   for (int i = 0; i < n; i++)
   {
     cin >> arr[i];
   }
-  
-  print(n, 0, r,  arr);
+
+  print(n, 0, arr);
   for (int i = 0; i < n; i++)
   {
     cout << arr[i] << " ";
