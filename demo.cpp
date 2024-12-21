e76bfda4e68cb3f4b97fbf9d26098733ba1af231
Durgesh shekhawat <durgeshshekhawat123456@gmail.com>
2024-12-21 16:21:41 +0530

    Implement selection sort algorithm.

--- demo.cpp
+++ demo.cpp
@@ -1,6 +1,26 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+void selection_sort(int n, int arr[])
+{
+  for (int i = 0; i <= n-2; i++)
+  {
+    int mini = i;
+
+    for (int j = i; j <= n - 1; j++)
+    {
+      if (arr[j] < arr[mini])
+      {
+        mini = j;
+      }
+      /*int temp = arr[mini];
+      arr[mini] = arr[i];
+      arr[i] = temp;*/
+      swap(arr[i], arr[mini]);  
+    } 
+  }
+}
+
 int main()
 {
   int n;
@@ -12,44 +32,10 @@ int main()
     cin >> arr[i];
   }
 
-  int hash[13] = {0};
+  selection_sort(n, arr);
 
   for (int i = 0; i < n; i++)
   {
-    hash[arr[i]]++;
+    cout << arr[i] << " ";
   }
-
-  int maxfreq = 0, maxelement, minfreq = INT_MAX, minelement;
-
-  for (int i = 0; i < 13; i++)
-  {
-    if (hash[i] >= maxfreq)
-    {
-      maxfreq = hash[i];
-      maxelement = i;
-    }
-
-    if (hash[i] > 0 && hash[i] <= minfreq)
-    {
-        minfreq = hash[i];
-        minelement = i;
-    }
-  }
-
-  cout << "Maximum Frequency Element is " << maxelement << endl;
-  cout << "Maximum Frequency is " << maxfreq << endl;
-
-  cout << "Minimum Frequency Element is " << minelement << endl;
-  cout << "Minimum Frequency is " << minfreq << endl;
-
-  int q;
-  cin >> q;
-  while (q--)
-  {
-    int number;
-    cin >> number;
-
-    cout << hash[number] << endl;
-  }
-  return 0;
 }
