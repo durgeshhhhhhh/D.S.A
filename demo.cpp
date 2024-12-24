a9b3b15d3e6b93c2938fb6aead71869c1905745f
Durgesh shekhawat <durgeshshekhawat123456@gmail.com>
2024-12-24 11:26:55 +0530

    Optimize bubble sort algorithm with early termination for sorted arrays.

--- demo.cpp
+++ demo.cpp
@@ -5,6 +5,7 @@ void bubbleSort(int n, int arr[])
 {
   for (int i = 0; i < n - 1; i++)
   {
+    bool swapped = false;
     for (int j = 0; j < n - i - 1; j++)
     {
       if (arr[j] > arr[j + 1])
@@ -14,7 +15,13 @@ void bubbleSort(int n, int arr[])
         int temp = arr[j];
         arr[j] = arr[j + 1];
         arr[j + 1] = temp;
+
+        swapped = true;
       }
+    }
+    if (!swapped)
+    {
+      break;
     }
   }
 }
