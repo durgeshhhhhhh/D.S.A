bf282fe66aba2cecebfdef8dcf5219f7d97991de
Durgesh shekhawat <durgeshshekhawat123456@gmail.com>
2025-01-08 18:48:30 +0530

    Find the Largest Element in an Array.

--- demo.cpp
+++ demo.cpp
@@ -1,43 +1,6 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int partIndex(vector<int> &arr, int low, int high)
-{
-  int pivot = arr[high];
-  int i = low;
-  int j = high;
-
-  while (i < j)
-  {
-    while (arr[j] >= pivot && j >= low + 1)
-    {
-      j--;
-    }
-
-    while (arr[i] < pivot && i <= high - 1)
-    {
-      i++;
-    }
-
-    if (i < j)
-      swap(arr[i], arr[j]);
-  }
-
-  swap(arr[i], arr[high]);
-  return i;
-}
-
-void quickSort(vector<int> &arr, int low, int high)
-{
-  if (low < high)
-  {
-    int partition = partIndex(arr, low, high);
-
-    quickSort(arr, low, partition - 1);
-    quickSort(arr, partition + 1, high);
-  }
-}
-
 int main()
 {
   int n;
@@ -50,12 +13,15 @@ int main()
     cin >> arr[i];
   }
 
-  quickSort(arr, 0, n - 1);
+  int max = arr[0];
 
   for (int i = 0; i < n; i++)
   {
-    cout << arr[i] << " ";
+    if (arr[i] > max)
+      max = arr[i];
   }
+
+  cout << "Largest element in an array is " << max;
 
   return 0;
 }
