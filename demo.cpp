e60210796cc908a24077a8021501c047797e56ae
Durgesh shekhawat <durgeshshekhawat123456@gmail.com>
2025-01-11 19:07:40 +0530

    Find missing Number (Brute force).

--- demo.cpp
+++ demo.cpp
@@ -1,60 +1,39 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-vector<int> findUnion(vector<int> &a, vector<int> &b)
+int missingNumber(vector<int> &arr)
 {
-  vector<int> intst;
-  int i = 0;
-  int j = 0;
-
-  while (i < a.size() && j < b.size())
+  for (int i = 1; i <= arr.size(); i++)
   {
-    if (a[i] < b[j])
+    int flag = 0;
+    for (int j = 0; j < arr.size(); j++)
     {
-      i++;
+      if (arr[j] == i)
+      {
+        flag = 1;
+        break;
+      }
     }
-    else if (a[i] > b[j])
-    {
-      j++;
-    }
-    else
-    {
-      intst.push_back(a[i]);
-      i++;
-      j++;
-    }
+    if (flag == 0)
+      return i;
   }
-  return intst;
+  return -1;
 }
 
 int main()
 {
   int n;
   cin >> n;
-  vector<int> a(n);
+  vector<int> arr(n);
 
   for (int i = 0; i < n; i++)
   {
-    cin >> a[i];
+    cin >> arr[i];
   }
 
-  int k;
-  cin >> k;
-  vector<int> b(k);
+  int result = missingNumber(arr);
 
-  for (int i = 0; i < k; i++)
-  {
-    cin >> b[i];
-  }
-
-  vector<int> result = findUnion(a, b);
-
-  cout << "::::: Intersection of two array :::::" << endl;
-
-  for (int i = 0; i < result.size(); i++)
-  {
-    cout << result[i] << " ";
-  }
+  cout << result;
 
   return 0;
 }
