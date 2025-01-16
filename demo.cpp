5d97f64dfdb2dccd6651e5065ba5637237b96bcd
Durgesh shekhawat <durgeshshekhawat123456@gmail.com>
2025-01-16 17:34:33 +0530

    Number appears once and other twice (better approach) [overcome the drawback of hash array].

--- demo.cpp
+++ demo.cpp
@@ -3,24 +3,16 @@ using namespace std;
 
 int numberAppearsOnce(vector<int> &arr, int n)
 {
-    int max = arr[0];
+    unordered_map<int, int> mpp;
     for (int i = 0; i < n; i++)
     {
-        if (arr[i] > max)
-            max = arr[i];
+        mpp[arr[i]]++;
     }
 
-    vector<int> hash(max + 1, 0);
-
-    for (int i = 0; i < n; i++)
+    for (auto it : mpp)
     {
-        hash[arr[i]]++;
-    }
-
-    for (int i = 0; i <= max; i++)
-    {
-        if (hash[i] == 1)
-            return i;
+        if (it.second == 1)
+            return it.first;
     }
 
     return -1;
