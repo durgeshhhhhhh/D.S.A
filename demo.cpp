c7bed439c2d8cfa13dfcf63dfb005da8f7d758a1
Durgesh shekhawat <durgeshshekhawat123456@gmail.com>
2025-01-20 14:06:35 +0530

    Fibonnaci Number (Brute force)

--- demo.cpp
+++ demo.cpp
@@ -6,16 +6,7 @@ int fibonnaci(int n)
     if (n < 2)
         return n;
 
-    vector<int> ans(n + 1);
-    ans[0] = 0;
-    ans[1] = 1;
-
-    for (int i = 2; i <= n; i++)
-    {
-        ans[i] = ans[i - 1] + ans[i - 2];
-    }
-
-    return ans[n];
+        return fibonnaci(n-1) + fibonnaci(n-2);
 }
 
 int main()
