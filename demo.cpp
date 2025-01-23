dd98894f480086c2d8e9e4cfde362b025914d94e
Durgesh shekhawat <durgeshshekhawat123456@gmail.com>
2025-01-23 17:10:10 +0530

    Majority Element (>n/2 times) [Better Approach]

--- demo.cpp
+++ demo.cpp
@@ -3,19 +3,17 @@ using namespace std;
 
 int majorityElement(vector<int> &arr, int n)
 {
+    map<int, int> mpp;
+
     for (int i = 0; i < n; i++)
     {
-        int cnt = 0;
+        mpp[arr[i]]++;
+    }
 
-        for (int j = 0; j < n; j++)
-        {
-            if (arr[j] == arr[i])
-            {
-                cnt++;
-            }
-        }
-        if (cnt > n / 2)
-            return arr[i];
+    for (auto it : mpp)
+    {
+        if (it.second > n / 2)
+            return it.first;
     }
 
     return -1;
