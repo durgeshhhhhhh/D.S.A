4c5c2d632df37c4459478859d162cf3227ff9233
Durgesh shekhawat <durgeshshekhawat123456@gmail.com>
2025-01-24 10:31:00 +0530

    Majority Element (>n/2 times) [Optimal Approach] (More's voting algorithm)

--- demo.cpp
+++ demo.cpp
@@ -3,20 +3,37 @@ using namespace std;
 
 int majorityElement(vector<int> &arr, int n)
 {
-    map<int, int> mpp;
+    int cnt = 0;
+    int el;
 
     for (int i = 0; i < n; i++)
     {
-        mpp[arr[i]]++;
+        if (cnt == 0)
+        {
+            cnt = 1;
+            el = arr[i];
+        }
+        else if (arr[i] == el)
+        {
+            cnt++;
+        }
+        else
+            cnt--;
     }
 
-    for (auto it : mpp)
+    int cnt1 = 0;
+    for (int i = 0; i < n; i++)
     {
-        if (it.second > n / 2)
-            return it.first;
+        if (arr[i] == el)
+            cnt1++;
     }
 
-    return -1;
+    if (cnt1 > n / 2)
+    {
+        return el;
+    }
+    else
+        return -1;
 }
 
 int main()
