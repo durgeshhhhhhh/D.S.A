99e6db50f2736e1e54963718011c0b5f022f610c
Durgesh shekhawat <durgeshshekhawat123456@gmail.com>
2025-01-24 13:49:12 +0530

    Maximum sub array sum ( Optimal approach ) [Kadanes Algorithm].

--- demo.cpp
+++ demo.cpp
@@ -3,17 +3,17 @@ using namespace std;
 
 int maximumSubarraySum(vector<int> &arr, int n)
 {
-    int maxi = INT_MIN;
-    for (int i = 0; i < n; i++)
-    {
+     int maxi = INT_MIN;
         int sum = 0;
-        for (int j = i; j < n; j++)
-        {
-            sum += arr[j];
-            maxi = max(sum, maxi);
+        for (int i = 0; i < n; i++) {
+            sum += arr[i];
+            maxi = max(maxi, sum);
+
+            if (sum < 0) {
+                sum = 0;
+            }
         }
-    }
-    return maxi;
+        return maxi;
 }
 
 int main()
