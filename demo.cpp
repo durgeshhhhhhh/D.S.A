799c66d8514696eebcadd8c9f67ca7d203938816
Durgesh shekhawat <durgeshshekhawat123456@gmail.com>
2025-01-24 19:55:44 +0530

    Maximum sub array sum ( Optimal approach with printing subArray ) [Kadanes Algorithm].

--- demo.cpp
+++ demo.cpp
@@ -1,19 +1,31 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int maximumSubarraySum(vector<int> &arr, int n)
+vector<int> maximumSubarraySum(vector<int> &arr, int n)
 {
-     int maxi = INT_MIN;
-        int sum = 0;
-        for (int i = 0; i < n; i++) {
-            sum += arr[i];
-            maxi = max(maxi, sum);
-
-            if (sum < 0) {
-                sum = 0;
-            }
+    int maxi = INT_MIN;
+    int sum = 0, ansStart = -1, ansEnd = -1, start;
+    for (int i = 0; i < n; i++)
+    {
+        if (sum == 0)
+        {
+            start = i;
         }
-        return maxi;
+
+        sum += arr[i];
+        if (sum > maxi)
+        {
+            maxi = sum;
+            ansStart = start;
+            ansEnd = i;
+        }
+
+        if (sum < 0)
+        {
+            sum = 0;
+        }
+    }
+    return {ansStart, ansEnd, maxi};
 }
 
 int main()
@@ -28,8 +40,18 @@ int main()
         cin >> arr[i];
     }
 
-    int result = maximumSubarraySum(arr, n);
-    cout << result;
+    vector<int> result = maximumSubarraySum(arr, n);
+
+    cout << ":::: Sub-Array ::::" << endl;
+    cout << "[";
+    for (int i = result[0]; i <= result[1]; i++)
+    {
+        cout << arr[i] << ",";
+    }
+    cout << "]";
+
+    cout << endl;
+    cout << "maximum is " << result[2];
 
     return 0;
 }
