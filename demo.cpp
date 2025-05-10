7e8528463d3c97291d2df66849331f04aa19ffaf
Durgesh Shekhawat <durgeshshekhawat123456@gmail.com>
2025-05-10 17:00:03 +0530

    print all divisors: practicing the concepts

--- demo.cpp
+++ demo.cpp
@@ -5,27 +5,13 @@ int main()
 {
     int n;
     cin >> n;
-    int cnt = 0;
-    int dup1 = n;
-    int dup2 = n;
-    int armstrong = 0;
 
-    while (n > 0)
+    for (int i = 1; i <= n; i++)
     {
-        n = n / 10;
-        cnt++;
-    }
-
-    while (dup1 > 0)
-    {
-        int lastDigit = dup1 % 10;
-        armstrong = pow(lastDigit, cnt) + armstrong;
-        dup1 = dup1 / 10;
-    }
-
-    if (armstrong == dup2)
-    {
-        cout << "Yes, it's a armstrong number";
+        if (n % i == 0)
+        {
+            cout << i << endl;
+        }
     }
 
     return 0;
