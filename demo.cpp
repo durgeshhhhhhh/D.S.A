e07c0e6e161d1f0b755a9a894a6642686224ade7
Durgesh Shekhawat <durgeshshekhawat123456@gmail.com>
2025-05-10 13:24:15 +0530

    check palindrome: practicing the concept

--- demo.cpp
+++ demo.cpp
@@ -4,17 +4,20 @@ using namespace std;
 int main()
 {
     int n;
-    int cnt = 0;
     cin >> n;
+    int dup = n;
+    int rev_no = 0;
 
-    cout << "Reverse of number is ";
     while (n > 0)
     {
         int lastDigit = n % 10;
-        cout << lastDigit;
+        rev_no = (rev_no * 10) + lastDigit;
+
         n = n / 10;
-        cnt++;
     }
-    cout << endl;
-    cout << "Number of digits: " << cnt;
+
+    if (rev_no == dup)
+        cout << "yes, it's a pallindrome";
+    else
+        cout << "No, it's not a pallindrome";
 }
