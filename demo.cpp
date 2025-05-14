28d27d653189855825d9756f83918be46a63661e
Durgesh Shekhawat <durgeshshekhawat123456@gmail.com>
2025-05-14 12:25:10 +0530

    reverse of an array: practicing the concepts

--- demo.cpp
+++ demo.cpp
@@ -1,12 +1,15 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int fact(int n)
+void reverse(int arr[], int i, int j)
 {
-    if (n == 1)
-        return 1;
 
-    return n * fact(n - 1);
+    if (i > j)
+        return;
+
+    swap(arr[i], arr[j]);
+
+    reverse(arr, i + 1, j - 1);
 }
 
 int main()
@@ -14,8 +17,17 @@ int main()
     int n;
     cin >> n;
 
-    int factorial = fact(n);
-    cout << factorial;
+    int arr[n];
 
-    return 0;
+    for (int i = 0; i < n; i++)
+    {
+        cin >> arr[i];
+    }
+
+    reverse(arr, 0, n - 1);
+
+    for (int i = 0; i < n; i++)
+    {
+        cout << arr[i] << " ";
+    }
 }
