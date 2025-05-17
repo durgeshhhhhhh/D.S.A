9e1317ddf0c83b89a55e7dd80b977ec444eb729f
Durgesh Shekhawat <durgeshshekhawat123456@gmail.com>
2025-05-17 14:03:43 +0530

    Bubble sort : Optimized version

--- demo.cpp
+++ demo.cpp
@@ -3,8 +3,9 @@ using namespace std;
 
 void bubble_sort(int arr[], int n)
 {
-    for (int i = 0; i <= n - 1; i++)
+    for (int i = 0; i < n - 1; i++)
     {
+        bool is_swapped = false;
         for (int j = 0; j < n - i - 1; j++)
         {
             if (arr[j] > arr[j + 1])
@@ -12,10 +13,16 @@ void bubble_sort(int arr[], int n)
                 int temp = arr[j];
                 arr[j] = arr[j + 1];
                 arr[j + 1] = temp;
+                is_swapped = true;
             }
         }
-    }   
-}           
+
+        if (!is_swapped)
+            break;
+
+        cout << "runs" << endl;
+    }
+}
 
 int main()
 {
