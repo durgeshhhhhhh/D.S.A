9db4e0214787fb4264cee3753e5355b033b03691
Durgesh Shekhawat <durgeshshekhawat123456@gmail.com>
2025-05-21 12:12:02 +0530

    largest element: practicing the concepts.

--- demo.cpp
+++ demo.cpp
@@ -1,73 +1,28 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-void merge(vector<int> &arr, int low, int mid, int high)
+int largest(int arr[], int n)
 {
-    int left = low;
-    int right = mid + 1;
-
-    vector<int> temp;
-
-    while (left <= mid && right <= high)
+    int maxx = arr[0];
+    for (int i = 0; i < n; i++)
     {
-        if (arr[left] <= arr[right])
-        {
-            temp.push_back(arr[left]);
-            left++;
-        }
-        else
-        {
-            temp.push_back(arr[right]);
-            right++;
-        }
+        if (arr[i] > maxx)
+            maxx = arr[i];
     }
-
-    while (left <= mid)
-    {
-        temp.push_back(arr[left]);
-        left++;
-    }
-
-    while (right <= high)
-    {
-        temp.push_back(arr[right]);
-        right++;
-    }
-
-    for (int i = low; i <= high; i++)
-    {
-        arr[i] = temp[i - low];
-    }
-}
-
-void mergeSort(vector<int> &arr, int low, int high)
-{
-    if (low >= high)
-        return;
-
-    int mid = (low + high) / 2;
-
-    mergeSort(arr, low, mid);
-    mergeSort(arr, mid + 1, high);
-
-    merge(arr, low, mid, high);
+                
+    return maxx;
 }
 
 int main()
 {
     int n;
     cin >> n;
-    vector<int> arr(n);
+    int arr[n];
 
     for (int i = 0; i < n; i++)
     {
         cin >> arr[i];
     }
 
-    mergeSort(arr, 0, n - 1);
-
-    for (int i = 0; i < n; i++)
-    {
-        cout << arr[i] << " ";
-    }
+    cout << largest(arr, n);
 }
