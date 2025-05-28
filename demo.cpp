322a8afb0040986f49239bfea157f6406fa27cf8
Durgesh Shekhawat <durgeshshekhawat123456@gmail.com>
2025-05-29 01:34:27 +0530

    union of two sorted arrray : brute force

--- demo.cpp
+++ demo.cpp
@@ -1,35 +1,54 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int linearSearch(vector<int> &arr, int n, int q)
+void findUnion(vector<int> &arr1, vector<int> &arr2, int n, int m)
 {
-    for (int i = arr.size() - 1; i >= 0; i--)
+    set<int> st;
+
+    for (int i = 0; i < n; i++)
     {
-        if (arr[i] == q)
-        {
-            return arr.size() - 1 - i;
-        }
+        st.insert(arr1[i]);
     }
 
-    return -1;
+    for (int i = 0; i < n; i++)
+    {
+        st.insert(arr2[i]);
+    }
+
+    vector<int> temp;
+
+    for (auto it : st)
+    {
+        temp.push_back(it);
+    }
+
+    for (int i = 0; i < temp.size(); i++)
+    {
+        cout << temp[i] << " ";
+    }
 }
 
 int main()
 {
     int n;
     cin >> n;
-
-    vector<int> arr(n);
+    vector<int> arr1(n);
 
     for (int i = 0; i < n; i++)
     {
-        cin >> arr[i];
+        cin >> arr1[i];
     }
 
-    int q;
-    cin >> q;
+    int m;
+    cin >> m;
+    vector<int> arr2(m);
 
-    cout << linearSearch(arr, n, q);
+    for (int i = 0; i < n; i++)
+    {
+        cin >> arr2[i];
+    }
+
+    findUnion(arr1, arr2, n, m);
 
     return 0;
 }
