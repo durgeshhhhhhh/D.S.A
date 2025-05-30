20583e89c70ffafbf59a4b820e668711e917797b
Durgesh Shekhawat <durgeshshekhawat123456@gmail.com>
2025-05-30 18:00:33 +0530

    MIssing Number : optimal approach ---> by XOR

--- demo.cpp
+++ demo.cpp
@@ -3,15 +3,19 @@ using namespace std;
 
 int missingNumber(vector<int> &arr, int n)
 {
-    int sum1 = (n * (n + 1)) / 2;
-
-    int sum2 = 0;
-    for (int i = 0; i < n - 1; i++)
+    int xor1 = 0;
+    for (int i = 1; i <= n; i++)
     {
-        sum2 += arr[i];
+        xor1 = xor1 ^ i;
     }
 
-    return sum1 - sum2;
+    int xor2 = 0;
+    for (int i = 0; i < n; i++)
+    {
+        xor2 = xor2 ^ arr[i];
+    }
+
+    return xor1 ^ xor2;
 }
 
 int main()
