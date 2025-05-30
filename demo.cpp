f209e5c6087cdd01a060c7ade88392c2df17b468
Durgesh Shekhawat <durgeshshekhawat123456@gmail.com>
2025-05-30 18:52:09 +0530

    Maximum consecutive ones : optimal

--- demo.cpp
+++ demo.cpp
@@ -1,20 +1,6 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int missingNumber(vector<int> &arr, int n)
-{
-    int xor1 = 0;
-    int xor2 = 0;
-    for (int i = 0; i < n - 1; i++)
-    {
-        xor2 = xor2 ^ arr[i];
-        xor1 = xor1 ^ (i + 1);
-    }
-    xor1 = xor1 ^ n;
-
-    return xor1 ^ xor2;
-}
-
 int main()
 {
     int n;
@@ -27,7 +13,23 @@ int main()
         cin >> arr[i];
     }
 
-    cout << missingNumber(arr, n);
+    int cnt = 0;
+    int maxi = 0;
+
+    for (int i = 0; i < n; i++)
+    {
+        if (arr[i] == 1)
+        {
+            cnt++;
+            maxi = max(cnt, maxi);
+        }
+        else
+        {
+            cnt = 0;
+        }
+    }
+
+    cout << maxi;
 
     return 0;
 }
