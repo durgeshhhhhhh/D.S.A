f7e91a8128d210204a3aa8d23f5719ac3e9133f4
Durgesh Shekhawat <durgeshshekhawat123456@gmail.com>
2025-06-05 13:23:17 +0530

    Maximum sub array sum : Better force

--- demo.cpp
+++ demo.cpp
@@ -6,15 +6,11 @@ int maximumSubarraySum(vector<int> &arr, int n)
     int maxx = INT_MIN;
     for (int i = 0; i < n; i++)
     {
-
+        int summation = 0;
         for (int j = i; j < n; j++)
         {
-            int summ = 0;
-            for (int k = i; k <= j; k++)
-            {
-                summ += arr[k];
-                maxx = max(maxx, summ);
-            }
+            summation += arr[j];
+            maxx = max(maxx, summation);
         }
     }
 
