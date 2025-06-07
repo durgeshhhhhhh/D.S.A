e911e283b582861790591547014c10b78c4e3ffa
Durgesh Shekhawat <durgeshshekhawat123456@gmail.com>
2025-06-07 13:23:16 +0530

    Next Permutation : Optimal Approach

--- demo.cpp
+++ demo.cpp
@@ -1,9 +1,60 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-void nextPermutation(vector<int> &arr)
+void nextPermutation(vector<int> &arr, int n)
 {
-    next_permutation(arr.begin(), arr.end());
+    int index = -1;
+    for (int i = n - 2; i >= 0; i--)
+    {
+        if (arr[i] < arr[i + 1])
+        {
+            index = i;      
+            break;
+        }
+    }
+
+    if (index == -1)
+    {
+        int i = 0;
+        int j = n - 1;
+
+        while (i < j)
+        {
+            int temp = arr[i];
+            arr[i] = arr[j];
+            arr[j] = temp;
+            i++;
+            j--;
+        }
+    }
+    else
+    {
+        for (int i = n - 1; i > index; i--)
+        {
+            cout << "new new" << endl;
+            if (arr[i] > arr[index])
+            {
+                int temp = arr[i];
+                arr[i] = arr[index];
+                arr[index] = temp;
+                break;
+            }
+        }
+
+        for (int i = index + 1; i > n; i++)
+        {
+            int j = n - 1;
+
+            while (i < j)
+            {
+                int temp = arr[i];
+                arr[i] = arr[j];
+                arr[j] = temp;
+                i++;
+                j--;
+            }
+        }
+    }
 }
 
 int main()
@@ -18,12 +69,10 @@ int main()
         cin >> arr[i];
     }
 
-    nextPermutation(arr);
+    nextPermutation(arr, n);
 
     for (int i = 0; i < n; i++)
     {
         cout << arr[i] << " ";
     }
-
-    return 0;
 }
