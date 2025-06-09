ae0510df6935f4863c071a0f13b55988cc8e7ee0
Durgesh Shekhawat <durgeshshekhawat123456@gmail.com>
2025-06-09 11:36:00 +0530

    Longest consecutive sequence : brute force

--- demo.cpp
+++ demo.cpp
@@ -1,29 +1,36 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-vector<int> leaders(vector<int> &arr, int n)
+bool ls(vector<int> arr, int num)
 {
-    vector<int> ans;
-
-    int maxx = INT_MIN;
-
-    int i = n - 1;
-
-    while (i >= 0)
+    int n = arr.size();
+    for (int i = 0; i < n; i++)
     {
-
-        if (arr[i] > maxx)
-        {
-            maxx = arr[i];
-            ans.push_back(arr[i]);
-        }
-
-        i--;
+        if (arr[i] == num)
+            return true;
     }
 
-    reverse(ans.begin(), ans.end());
+    return false;
+}
 
-    return ans;
+int longestConsecutives(vector<int> &arr, int n)
+{
+    int len = 1;
+    for (int i = 0; i < n; i++)
+    {
+        int cnt = 1;
+        int x = arr[i];
+
+        while (ls(arr, x + 1) == true)
+        {
+            x = x + 1;
+            cnt++;
+        }
+
+        len = max(len, cnt);
+    }
+
+    return len;
 }
 
 int main()
@@ -38,10 +45,5 @@ int main()
         cin >> arr[i];
     }
 
-    vector<int> result = leaders(arr, n);
-
-    for (int i = 0; i < result.size(); i++)
-    {
-        cout << result[i] << " ";
-    }
+    cout << longestConsecutives(arr, n);
 }
