427c7d0cafa89bcb5105ea12ea9b9f589a253abf
Durgesh Shekhawat <durgeshshekhawat123456@gmail.com>
2025-06-10 19:36:44 +0530

    Set Matrix Zero : Optimal Approach

--- demo.cpp
+++ demo.cpp
@@ -1,10 +1,11 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-void setZeroes(vector<vector<int>> &matrix, int n, int m)
+void setZeroes(vector<vector<int>> &matrix)
 {
-    vector<int> row(n, 0);
-    vector<int> col(m, 0);
+    int n = matrix.size();
+    int m = matrix[0].size();
+    int col0 = 1;
 
     for (int i = 0; i < n; i++)
     {
@@ -12,21 +13,44 @@ void setZeroes(vector<vector<int>> &matrix, int n, int m)
         {
             if (matrix[i][j] == 0)
             {
-                row[i] = 1;
-                col[j] = 1;
+                matrix[i][0] = 0;
+
+                if (j != 0)
+                    matrix[0][j] = 0;
+                else
+                    col0 = 0;
             }
         }
     }
 
-    for (int i = 0; i < n; i++)
+    for (int i = 1; i < n; i++)
+    {
+        for (int j = 1; j < m; j++)
+        {
+            if (matrix[i][j] != 0)
+            {
+                if (matrix[i][0] == 0 || matrix[0][j] == 0)
+                {
+                    matrix[i][j] = 0;
+                }
+            }
+        }
+    }
+
+    if (matrix[0][0] == 0)
     {
         for (int j = 0; j < m; j++)
         {
-            if (row[i] || col[j])
-            {
-                matrix[i][j] = 0;
-            }
-        }   
+            matrix[0][j] = 0;
+        }
+    }
+
+    if (col0 == 0)
+    {
+        for (int i = 0; i < n; i++)
+        {
+            matrix[i][0] = 0;
+        }
     }
 }
 
@@ -36,6 +60,7 @@ int main()
     cin >> n;
     int m;
     cin >> m;
+
     vector<vector<int>> matrix(n, vector<int>(m));
 
     for (int i = 0; i < n; i++)
@@ -46,7 +71,7 @@ int main()
         }
     }
 
-    setZeroes(matrix, n, m);
+    setZeroes(matrix);
 
     for (int i = 0; i < n; i++)
     {
@@ -54,6 +79,7 @@ int main()
         {
             cout << matrix[i][j] << "   ";
         }
+
         cout << endl;
     }
 
