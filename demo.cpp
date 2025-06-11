f358eafa50b90dfca03aa1ad423776e52df3b871
Durgesh Shekhawat <durgeshshekhawat123456@gmail.com>
2025-06-11 10:18:57 +0530

    Set Matrix Zero (Better Approach) ----> Practice

--- demo.cpp
+++ demo.cpp
@@ -1,34 +1,19 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-void markRow(vector<vector<int>> &matrix, int i, int m)
-{
-    for (int j = 0; j < m; j++)
-    {
-        if (matrix[i][j] != 0)
-            matrix[i][j] = -1;
-    }
-}
-
-void markCol(vector<vector<int>> &matrix, int j, int n)
-{
-    for (int i = 0; i < n; i++)
-    {
-        if (matrix[i][j] != 0)
-            matrix[i][j] = -1;
-    }
-}
-
 void setZeroes(vector<vector<int>> &matrix, int n, int m)
 {
+    vector<int> row(n, 0);
+    vector<int> col(m, 0);
+
     for (int i = 0; i < n; i++)
     {
         for (int j = 0; j < m; j++)
         {
             if (matrix[i][j] == 0)
             {
-                markRow(matrix, i, m);
-                markCol(matrix, j, n);
+                row[i] = 1;
+                col[j] = 1;
             }
         }
     }
@@ -37,8 +22,10 @@ void setZeroes(vector<vector<int>> &matrix, int n, int m)
     {
         for (int j = 0; j < m; j++)
         {
-            if (matrix[i][j] == -1)
+            if (row[i] || col[j])
+            {
                 matrix[i][j] = 0;
+            }
         }
     }
 }
