00bffcd16f3b73700b53097803f90cee29aac730
Durgesh Shekhawat <durgeshshekhawat123456@gmail.com>
2025-06-11 11:21:57 +0530

    Set Matrix Zero (Optimal Approach) ----> Practice

--- demo.cpp
+++ demo.cpp
@@ -3,30 +3,47 @@ using namespace std;
 
 void setZeroes(vector<vector<int>> &matrix, int n, int m)
 {
-    vector<int> row(n, 0);
-    vector<int> col(m, 0);
-
+    int col0 = 1;
     for (int i = 0; i < n; i++)
     {
         for (int j = 0; j < m; j++)
         {
             if (matrix[i][j] == 0)
             {
-                row[i] = 1;
-                col[j] = 1;
+                matrix[i][0] = 0;
+
+                if (j != 0)
+                    matrix[0][j] = 0;
+                else
+                    col0 = 0;
             }
         }
     }
 
-    for (int i = 0; i < n; i++)
+    for (int i = 1; i < n; i++)
     {
-        for (int j = 0; j < m; j++)
+        for (int j = 1; j < m; j++)
         {
-            if (row[i] || col[j])
+            if (matrix[i][j] != 0)
             {
-                matrix[i][j] = 0;
+                if (matrix[i][0] == 0 || matrix[0][j] == 0)
+                    matrix[i][j] = 0;
             }
         }
+    }
+
+    if (matrix[0][0] == 0)
+    {
+        for (int j = 0; j < m; j++)
+
+            matrix[0][j] = 0;
+    }
+
+    if (col0 == 0)
+    {
+        for (int i = 0; i < n; i++)
+
+            matrix[i][0] = 0;
     }
 }
 
@@ -35,12 +52,11 @@ int main()
     int n, m;
     cin >> n >> m;
 
-    // int matrix[4][4];
     vector<vector<int>> matrix(n, vector<int>(m));
 
     for (int i = 0; i < n; i++)
     {
-        for (int j = 0; j < n; j++)
+        for (int j = 0; j < m; j++)
         {
             cin >> matrix[i][j];
         }
@@ -50,7 +66,7 @@ int main()
 
     for (int i = 0; i < n; i++)
     {
-        for (int j = 0; j < n; j++)
+        for (int j = 0; j < m; j++)
         {
             cout << matrix[i][j] << "   ";
         }
