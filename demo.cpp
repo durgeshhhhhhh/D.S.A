8a538780e110a555b572027e2cecf3b1dd14a672
Durgesh Shekhawat <durgeshshekhawat123456@gmail.com>
2025-06-11 18:24:05 +0530

    Rotate Matrix by 90 degree : Optimal Approach

--- demo.cpp
+++ demo.cpp
@@ -1,19 +1,21 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-vector<vector<int>> rotateMatrix(vector<vector<int>> &matrix, int n)
+void rotateMatrix(vector<vector<int>> &matrix, int n)
 {
-    vector<vector<int>> ans(n, vector<int>(n));
 
-    for (int i = 0; i < n; i++)
+    for (int i = 0; i < n - 1; i++)
     {
-        for (int j = 0; j < n; j++)
+        for (int j = i + 1; j < n; j++)
         {
-            ans[j][n - 1 - i] = matrix[i][j];
+            swap(matrix[i][j], matrix[j][i]);
         }
     }
 
-    return ans;
+    for (int i = 0; i < n; i++)
+    {
+        reverse(matrix[i].begin(), matrix[i].end());
+    }
 }
 
 int main()
@@ -31,13 +33,13 @@ int main()
         }
     }
 
-    vector<vector<int>> result = rotateMatrix(matrix, n);
+    rotateMatrix(matrix, n);
 
     for (int i = 0; i < n; i++)
     {
         for (int j = 0; j < n; j++)
         {
-            cout << result[i][j] << "  ";
+            cout << matrix[i][j] << "  ";
         }
         cout << endl;
     }
