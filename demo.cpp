2f7757725f981db630e51cf218dc71f6b0cf02e5
Durgesh Shekhawat <durgeshshekhawat123456@gmail.com>
2025-06-11 09:59:01 +0530

    Set Matrix Zero (Brute Force) ----> Practice

--- demo.cpp
+++ demo.cpp
@@ -1,85 +1,72 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-void setZeroes(vector<vector<int>> &matrix)
+void markRow(vector<vector<int>> &matrix, int i, int m)
 {
-    int n = matrix.size();
-    int m = matrix[0].size();
-    int col0 = 1;
+    for (int j = 0; j < m; j++)
+    {
+        if (matrix[i][j] != 0)
+            matrix[i][j] = -1;
+    }
+}
 
+void markCol(vector<vector<int>> &matrix, int j, int n)
+{
+    for (int i = 0; i < n; i++)
+    {
+        if (matrix[i][j] != 0)
+            matrix[i][j] = -1;
+    }
+}
+
+void setZeroes(vector<vector<int>> &matrix, int n, int m)
+{
     for (int i = 0; i < n; i++)
     {
         for (int j = 0; j < m; j++)
         {
             if (matrix[i][j] == 0)
             {
-                matrix[i][0] = 0;
-
-                if (j != 0)
-                    matrix[0][j] = 0;
-                else
-                    col0 = 0;
+                markRow(matrix, i, m);
+                markCol(matrix, j, n);
             }
         }
     }
 
-    for (int i = 1; i < n; i++)
-    {
-        for (int j = 1; j < m; j++)
-        {
-            if (matrix[i][j] != 0)
-            {
-                if (matrix[i][0] == 0 || matrix[0][j] == 0)
-                {
-                    matrix[i][j] = 0;
-                }
-            }
-        }
-    }
-
-    if (matrix[0][0] == 0)
+    for (int i = 0; i < n; i++)
     {
         for (int j = 0; j < m; j++)
         {
-            matrix[0][j] = 0;
-        }
-    }
-
-    if (col0 == 0)
-    {
-        for (int i = 0; i < n; i++)
-        {
-            matrix[i][0] = 0;
+            if (matrix[i][j] == -1)
+                matrix[i][j] = 0;
         }
     }
 }
 
 int main()
 {
-    int n;
-    cin >> n;
-    int m;
-    cin >> m;
+    int n, m;
+    cin >> n >> m;
 
+    // int matrix[4][4];
     vector<vector<int>> matrix(n, vector<int>(m));
 
     for (int i = 0; i < n; i++)
     {
-        for (int j = 0; j < m; j++)
+        for (int j = 0; j < n; j++)
         {
             cin >> matrix[i][j];
         }
     }
 
-    setZeroes(matrix);
+    setZeroes(matrix, n, m);
 
     for (int i = 0; i < n; i++)
     {
-        for (int j = 0; j < m; j++)
+        for (int j = 0; j < n; j++)
         {
             cout << matrix[i][j] << "   ";
         }
-
         cout << endl;
     }
 
