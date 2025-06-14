dd638271ee11f8ed8502df5c8d1392e80ea7a2ea
Durgesh Shekhawat <durgeshshekhawat123456@gmail.com>
2025-06-14 18:51:43 +0530

    Majority Element II : optimal approach ----> Moore's voting algorithm

--- demo.cpp
+++ demo.cpp
@@ -3,19 +3,50 @@ using namespace std;
 
 vector<int> majorityElement(vector<int> &arr, int n)
 {
-    vector<int> ans;
-    map<int, int> mpp;
-    int minimum = (n / 3) + 1;
+    int cnt1 = 0;
+    int cnt2 = 0;
+    int el1;
+    int el2;
 
     for (int i = 0; i < n; i++)
     {
-        mpp[arr[i]]++;
-
-        if (mpp[arr[i]] == minimum)
+        if (cnt1 == 0 && arr[i] != el2)
         {
-            ans.push_back(arr[i]);
+            cnt1 = 1;
+            el1 = arr[i];
+        }
+        else if (cnt2 == 0 && arr[i] != el1)
+        {
+            cnt2 = 1;
+            el2 = arr[i];
+        }
+        else if (arr[i] == el1)
+            cnt1++;
+        else if (arr[i] == el2)
+            cnt2++;
+        else
+        {
+            cnt1--;
+            cnt2--;
         }
     }
+
+    vector<int> ans;
+    cnt1 = 0;
+    cnt2 = 0;
+
+    for (int i = 0; i < n; i++)
+    {
+        if (arr[i] == el1)
+            cnt1++;
+        if (arr[i] == el2)
+            cnt2++;
+    }
+
+    if (cnt1 > n / 3)
+        ans.push_back(el1);
+    if (cnt2 > n / 3)
+        ans.push_back(el2);
 
     return ans;
 }
