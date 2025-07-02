e45653a916a1f31635f4e927e25f1f3f069f60e5
Durgesh Shekhawat <durgeshshekhawat123456@gmail.com>
2025-07-02 12:26:35 +0530

    3Sum : brute force approach

--- demo.cpp
+++ demo.cpp
@@ -1,53 +1,26 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-vector<int> majorityElement(vector<int> &arr, int n)
+vector<vector<int>> threeSum(int n, vector<int> &arr)
 {
-    int cnt1 = 0;
-    int cnt2 = 0;
-    int el1;
-    int el2;
-
+    set<vector<int>> st;
     for (int i = 0; i < n; i++)
     {
-        if (cnt1 == 0 && arr[i] != el2)
+        for (int j = i + 1; j < n; j++)
         {
-            cnt1 = 1;
-            el1 = arr[i];
-        }
-        else if (cnt2 == 0 && arr[i] != el1)
-        {
-            cnt2 = 1;
-            el2 = arr[i];
-        }
-        else if (arr[i] == el1)
-            cnt1++;
-        else if (arr[i] == el2)
-            cnt2++;
-        else
-        {
-            cnt1--;
-            cnt2--;
+            for (int k = j + 1; k < n; k++)
+            {
+                if (arr[i] + arr[j] + arr[k] == 0)
+                {
+                    vector<int> temp = {arr[i], arr[j], arr[k]};
+                    sort(temp.begin(), temp.end());
+                    st.insert(temp);
+                }
+            }
         }
     }
 
-    vector<int> ans;
-    cnt1 = 0;
-    cnt2 = 0;
-
-    for (int i = 0; i < n; i++)
-    {
-        if (arr[i] == el1)
-            cnt1++;
-        if (arr[i] == el2)
-            cnt2++;
-    }
-
-    if (cnt1 > n / 3)
-        ans.push_back(el1);
-    if (cnt2 > n / 3 && el2 != el1)
-        ans.push_back(el2);
-
+    vector<vector<int>> ans(st.begin(), st.end());
     return ans;
 }
 
@@ -63,11 +36,15 @@ int main()
         cin >> arr[i];
     }
 
-    vector<int> result = majorityElement(arr, n);
+    vector<vector<int>> result = threeSum(n, arr);
 
     for (int i = 0; i < result.size(); i++)
     {
-        cout << result[i] << " ";
+        for (int j = 0; j < result[i].size(); j++)
+        {
+            cout << result[i][j] << " ";
+        }
+        cout << endl;
     }
 
     return 0;
