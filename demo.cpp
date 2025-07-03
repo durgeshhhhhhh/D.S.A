dd21d5cff621f8877d3cfd16dfeceeea283e2e30
Durgesh Shekhawat <durgeshshekhawat123456@gmail.com>
2025-07-03 14:54:09 +0530

    3Sum : better approach

--- demo.cpp
+++ demo.cpp
@@ -6,20 +6,19 @@ vector<vector<int>> threeSum(int n, vector<int> &arr)
     set<vector<int>> st;
     for (int i = 0; i < n; i++)
     {
+        set<int> hashset;
         for (int j = i + 1; j < n; j++)
         {
-            for (int k = j + 1; k < n; k++)
+            int third = -(arr[i] + arr[j]);
+            if (hashset.find(third) != hashset.end())
             {
-                if (arr[i] + arr[j] + arr[k] == 0)
-                {
-                    vector<int> temp = {arr[i], arr[j], arr[k]};
-                    sort(temp.begin(), temp.end());
-                    st.insert(temp);
-                }
+                vector<int>temp = {arr[i], arr[j], third};
+                sort(temp.begin(), temp.end());
+                st.insert(temp);
             }
+            hashset.insert(arr[j]);
         }
     }
-
     vector<vector<int>> ans(st.begin(), st.end());
     return ans;
 }
