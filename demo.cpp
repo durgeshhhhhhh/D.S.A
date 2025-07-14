6b182ebb2e2d40f8106a8a90b19f752896d2dbc3
Durgesh Shekhawat <durgeshshekhawat123456@gmail.com>
2025-07-14 12:26:04 +0530

    Practice : Count digits

--- demo.cpp
+++ demo.cpp
@@ -1,69 +1,26 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-vector<vector<int>> threeSum(int n, vector<int> &arr)
-{
-    sort(arr.begin(), arr.end());
-    vector<vector<int>> ans;
-
-    for (int i = 0; i < n; i++)
-    {
-        while (i > 0 && arr[i] == arr[i - 1])
-            continue;
-
-        int j = i + 1;
-        int k = n - 1;
-
-        while (j < k)
-        {
-            int sum = arr[i] + arr[j] + arr[k];
-
-            if (sum < 0)
-            {
-                j++;
-            }
-            else if (sum > 0)
-            {
-                k--;
-            }
-            else
-            {
-                vector<int> temp = {arr[i], arr[j], arr[k]};
-                ans.push_back(temp);
-                j++;
-                k--;
-                while (j < k && arr[j] == arr[j - 1])
-                    j++;
-                while (j < k && arr[k] == arr[k + 1])
-                    k--;
-            }
-        }
-    }
-    return ans;
-}
-
 int main()
 {
     int n;
     cin >> n;
 
-    vector<int> arr(n);
-
-    for (int i = 0; i < n; i++)
+    int cnt = 0;
+    if (n == 0)
     {
-        cin >> arr[i];
+        cnt = 1;
     }
-
-    vector<vector<int>> result = threeSum(n, arr);
-
-    for (int i = 0; i < result.size(); i++)
+    else
     {
-        for (int j = 0; j < result[i].size(); j++)
+        while (n > 0)
         {
-            cout << result[i][j] << " ";
+            cnt++;
+            n = n / 10;
         }
-        cout << endl;
     }
+
+    cout << cnt;
 
     return 0;
 }
