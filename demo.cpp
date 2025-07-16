b595e4c39b0a2d18bcfe5be1ea1b0532193da48a
Durgesh Shekhawat <durgeshshekhawat123456@gmail.com>
2025-07-16 10:14:10 +0530

    Practice : Armstrong Number.

--- demo.cpp
+++ demo.cpp
@@ -3,23 +3,37 @@ using namespace std;
 
 int main()
 {
-    int n1;
-    cin >> n1;
+    int n;
+    cin >> n;
 
-    int n2;
-    cin >> n2;
+    int original_number = n;
+    int number = n;
 
-    int gcd = 1;
+    int cnt = 0;
 
-    for (int i = min(n1, n2); i >= 1; i--)
+    while (n > 0)
     {
-        if (n1 % i == 0 && n2 % i == 0)
-        {
-            gcd = i;
-            break;
-        }
+        cnt++;
+        n = n / 10;
     }
-    cout << gcd;
+
+    int new_number = 0;
+
+    while (original_number > 0)
+    {
+        int rem = original_number % 10;
+
+        new_number += pow(rem, cnt);
+
+        original_number = original_number / 10;
+    }
+
+    cout << new_number << endl;
+
+    if (number == new_number)
+        cout << "Yes! it's an Armstrong";
+    else
+        cout << "No! it's not an Armstrong";
 
     return 0;
 }
