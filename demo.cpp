e4fe2e30e2d310de557bfcf0aa5ec16d94d5cb48
Durgesh Shekhawat <durgeshshekhawat123456@gmail.com>
2025-07-16 10:49:22 +0530

    Practice : Prime Number.

--- demo.cpp
+++ demo.cpp
@@ -6,16 +6,29 @@ int main()
     int n;
     cin >> n;
 
-    for (int i = 1; i <= sqrt(n); i++)
+    if (n <= 1)
+    {
+        cout << "It's not a prime number.";
+        return 0;
+    }
+
+    int cnt = 0;
+
+    for (int i = 1; i * i <= n; i++)
     {
         if (n % i == 0)
         {
-            if (i == n / i)
-                cout << i;
-            else
-                cout << i << " " << n / i << " ";
+            cnt++;
+
+            if (n / i != i)
+                cnt++;
         }
     }
+
+    if (cnt == 2)
+        cout << "It's a prime number.";
+    else
+        cout << "It's not a prime number.";
 
     return 0;
 }
