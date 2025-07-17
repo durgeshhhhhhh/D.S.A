4186e4964727f40d13ab236fa184fd414315ab86
Durgesh Shekhawat <durgeshshekhawat123456@gmail.com>
2025-07-17 12:01:27 +0530

    Implement Upper Bound : by upper_bound() function

--- demo.cpp
+++ demo.cpp
@@ -1,29 +1,6 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-/*int lowerBound(vector<int> &nums, int x)
-{
-    int n = nums.size();
-    int low = 0;
-    int high = n - 1;
-    int ans = n;
-
-    while (low <= high)
-    {
-        int mid = low + (high - low) / 2;
-
-        if (nums[mid] >= x)
-        {
-            ans = mid;
-            high = mid - 1;
-        }
-        else
-            low = mid + 1;
-    }
-
-    return ans;
-}*/
-
 int main()
 {
     int n;
@@ -39,7 +16,7 @@ int main()
     int x;
     cin >> x;
 
-    cout << lower_bound(nums.begin(), nums.end(),x) - nums.begin();
+    cout << upper_bound(nums.begin(), nums.end(),x) - nums.begin();
 
     return 0;
 }
