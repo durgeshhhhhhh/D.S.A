be67b1824a527129b148e71e93611c428ec3e272
Durgesh Shekhawat <durgeshshekhawat123456@gmail.com>
2025-08-01 13:58:00 +0530

    Find First and Last Position of Element in Sorted Array : Optimal Approach

--- demo.cpp
+++ demo.cpp
@@ -1,29 +1,53 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int binarySearch(vector<int> &nums, int target)
+vector<int> searchRange(vector<int> &nums, int target)
 {
     int n = nums.size();
     int low = 0;
     int high = n - 1;
-    int ans = -1;
+    int i = -1;
+    int j = -1;
 
     while (low <= high)
     {
         int mid = (low + high) / 2;
 
-        if (nums[mid] <= target)
+        if (nums[mid] == target)
         {
-            ans = nums[mid];
-            low = mid + 1;
-        }
-        else
-        {
+            i = mid;
             high = mid - 1;
         }
+        else if (nums[mid] > target)
+            high = mid - 1;
+        else
+            low = mid + 1;
     }
 
-    return ans;
+    if (i == -1)
+    {
+        return {i, j};
+    }
+
+    low = 0;
+    high = n - 1;
+
+    while (low <= high)
+    {
+        int mid = (low + high) / 2;
+
+        if (nums[mid] == target)
+        {
+            j = mid;
+            low = mid + 1;
+        }
+        else if (nums[mid] < target)
+            low = mid + 1;
+        else
+            high = mid - 1;
+    }
+
+    return {i, j};
 }
 
 int main()
@@ -41,7 +65,9 @@ int main()
     int target;
     cin >> target;
 
-    cout << binarySearch(nums, target);
+    vector<int> result = searchRange(nums, target);
+
+    cout << "[" << result[0] << " , " << result[1] << "]";
 
     /*for (int i = 0; i < n; i++)
     {
