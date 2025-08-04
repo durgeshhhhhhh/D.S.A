63ab8c1feb9d0bebccdd285b12e95917254b3e7b
Durgesh Shekhawat <durgeshshekhawat123456@gmail.com>
2025-08-04 10:17:04 +0530

    search in rotated sorted array : Optimal Solution

--- demo.cpp
+++ demo.cpp
@@ -1,18 +1,34 @@
-#include <bits/stdc++.h>
+#include <bits/stdc++.h>;
 using namespace std;
 
 int search(vector<int> &nums, int target)
 {
     int n = nums.size();
+    int low = 0;
+    int high = n - 1;
 
-    for (int i = 0; i < n; i++)
+    while (low <= high)
     {
-        if (nums[i] == target)
+        int mid = low + (high - low) / 2;
+
+        if (nums[mid] == target)
+            return mid;
+
+        if (nums[low] <= nums[mid])
         {
-            return i;
+            if (nums[low] <= target && target <= nums[mid])
+                high = mid - 1;
+            else
+                low = mid + 1;
+        }
+        else
+        {
+            if (nums[mid] <= target && target <= nums[high])
+                low = mid + 1;
+            else
+                high = mid - 1;
         }
     }
-
     return -1;
 }
 
@@ -32,11 +48,6 @@ int main()
     cin >> target;
 
     cout << search(nums, target);
-
-    /*for (int i = 0; i < n; i++)
-    {
-        cout << nums[i] << " ";
-    }*/
 
     return 0;
 }
