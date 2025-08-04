819ab2d89141437f6ce9facf251c084b33787d84
Durgesh Shekhawat <durgeshshekhawat123456@gmail.com>
2025-08-04 19:51:39 +0530

    Revision : print all divisors

--- demo.cpp
+++ demo.cpp
@@ -3,17 +3,17 @@ using namespace std;
 
 int main()
 {
-    int n1;
-    int n2;
-    cin >> n1;
-    cin >> n2;
+    int n;
+    cin >> n;
 
-    for (int i = min(n1, n2); i >= 1; i--)
+    for (int i = 1; i * i <= n; i++)
     {
-        if (n1 % i == 0 && n2 % i == 0)
+        if (n % i == 0)
         {
-            cout << i;
-            break;
+            cout << i << endl;
+
+            if (i != n / i)
+                cout << n / i << endl;
         }
     }
 
