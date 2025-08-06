469aa618b3c3568088aac896fafa7f3298e37309
Durgesh Shekhawat <durgeshshekhawat123456@gmail.com>
2025-08-06 10:32:45 +0530

    Find out how many times has an array been rotated (with Duplicates)

--- demo.cpp
+++ demo.cpp
@@ -13,15 +13,27 @@ int findKRotation(vector<int> &nums)
     {
         int mid = low + (high - low) / 2;
 
-        if (nums[low] <= nums[high])
+        if (nums[low] < nums[high])
         {
             // ans = min(ans, nums[low]);
             if (nums[low] < ans)
             {
-                index = low;
                 ans = nums[low];
+                index = low;
             }
             break;
+        }
+
+        if (nums[low] == nums[mid] && nums[mid] == nums[high])
+        {
+            if (nums[low] < ans)
+            {
+                ans = nums[low];
+                index = low;
+            }
+            low++;
+            high--;
+            continue;
         }
 
         if (nums[low] <= nums[mid])
@@ -29,18 +41,18 @@ int findKRotation(vector<int> &nums)
             // ans = min(ans, nums[low]);
             if (nums[low] < ans)
             {
-                index = low;
                 ans = nums[low];
+                index = low;
             }
             low = mid + 1;
         }
         else
         {
             // ans = min(ans, nums[mid]);
-            if (nums[low] < ans)
+            if (nums[mid] < ans)
             {
-                index = mid;
                 ans = nums[mid];
+                index = mid;
             }
             high = mid - 1;
         }
