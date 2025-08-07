3d4fe3be05f01445625851c63d4083641b62bc74
Durgesh Shekhawat <durgeshshekhawat123456@gmail.com>
2025-08-07 11:27:33 +0530

    Find Peak Element : Brute force

--- demo.cpp
+++ demo.cpp
@@ -1,40 +1,25 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int singleNonDuplicate(vector<int> &nums)
+int peakElement(vector<int> &nums)
 {
     int n = nums.size();
-
-    if (n == 1)
-        return nums[0];
-
-    if (nums[0] != nums[1])
+    for (int i = 0; i < n; i++)
     {
-        return nums[0];
-    }
-    else if (nums[n - 1] != nums[n - 2])
-    {
-        return nums[n - 1];
-    }
-
-    int low = 1;
-    int high = n - 2;
-
-    while (low <= high)
-    {
-        int mid = low + (high - low) / 2;
-
-        if (nums[mid] != nums[mid - 1] && nums[mid] != nums[mid + 1])
+        if (i == 0 && nums[i + 1] < nums[i])
         {
-            return nums[mid];
+            return nums[i];
         }
-        else if ((mid % 2 == 1 && nums[mid] == nums[mid - 1]) || (mid % 2 == 0 && nums[mid] == nums[mid + 1]))
+        else if (i == n - 1 && nums[i] > nums[i - 1])
         {
-            low = mid + 1;
+            return nums[i];
         }
         else
         {
-            high = mid - 1;
+            if (nums[i] > nums[i + 1] && nums[i] > nums[i - 1])
+            {
+                return nums[i];
+            }
         }
     }
 
@@ -53,7 +38,7 @@ int main()
         cin >> nums[i];
     }
 
-    cout << singleNonDuplicate(nums);
+    cout << peakElement(nums);
 
     return 0;
 }
