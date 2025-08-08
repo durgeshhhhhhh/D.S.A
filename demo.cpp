76fa0c5f83b96cbd239c2505db5409f2d6334d22
Durgesh Shekhawat <durgeshshekhawat123456@gmail.com>
2025-08-08 10:50:33 +0530

    Find Peak Element : little optimized brute force

--- demo.cpp
+++ demo.cpp
@@ -6,21 +6,8 @@ int peakElement(vector<int> &nums)
     int n = nums.size();
     for (int i = 0; i < n; i++)
     {
-        if (i == 0 && nums[i + 1] < nums[i])
-        {
-            return nums[i];
-        }
-        else if (i == n - 1 && nums[i] > nums[i - 1])
-        {
-            return nums[i];
-        }
-        else
-        {
-            if (nums[i] > nums[i + 1] && nums[i] > nums[i - 1])
-            {
-                return nums[i];
-            }
-        }
+        if ((i == 0 || nums[i] > nums[i - 1]) && (i == n - 1 || nums[i] > nums[i + 1]))
+            return i;
     }
 
     return -1;
