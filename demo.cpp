92342e115b3b97dede1d3474f014c493eb5f25e7
Durgesh Shekhawat <durgeshshekhawat123456@gmail.com>
2025-08-08 12:00:27 +0530

    Find Peak Element (if only one exists) : optimal approach

--- demo.cpp
+++ demo.cpp
@@ -4,10 +4,34 @@ using namespace std;
 int peakElement(vector<int> &nums)
 {
     int n = nums.size();
-    for (int i = 0; i < n; i++)
+
+    if (n == 1)
+        return 0;
+
+    if (nums[0] > nums[1])
+        return 0;
+    else if (nums[n - 1] > nums[n - 2])
+        return n - 1;
+
+    int low = 1;
+    int high = n - 2;
+
+    while (low <= high)
     {
-        if ((i == 0 || nums[i] > nums[i - 1]) && (i == n - 1 || nums[i] > nums[i + 1]))
-            return i;
+        int mid = low + (high - low) / 2;
+
+        if ((nums[mid] > nums[mid - 1]) && (nums[mid] > nums[mid + 1]))
+        {
+            return mid;
+        }
+        else if (nums[mid] > nums[mid - 1])
+        {
+            low = mid + 1;
+        }
+        else
+        {
+            high = mid - 1;
+        }
     }
 
     return -1;
