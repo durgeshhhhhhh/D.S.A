cb5e37feb718eb38a47a0ed3a3647e84b87f3d3e
Durgesh Shekhawat <durgeshshekhawat123456@gmail.com>
2025-08-12 20:04:07 +0530

    Print name N times using recursion

--- demo.cpp
+++ demo.cpp
@@ -1,49 +1,19 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int sumOfDivision(vector<int> &nums, int value)
+void printName(int n)
 {
-    int add = 0;
-    for (int i = 0; i < nums.size(); i++)
-    {
-        add += (nums[i] + value - 1) / value;
-    }
 
-    return add;
-}
-
-int smallestDivisor(vector<int> &nums, int threshold)
-{
-    int n = nums.size();
-
-    int maxx = *max_element(nums.begin(), nums.end());
-
-    for (int i = 1; i <= maxx; i++)
-    {
-        if (sumOfDivision(nums, i) <= threshold)
-        {
-            return i;
-        }
-    }
-    return -1;
-}
+    if (n < 1)
+        return;
+    cout << "Durgesh Shekhawat" << endl;
+    printName(n - 1);
+};
 
 int main()
 {
     int n;
     cin >> n;
 
-    vector<int> nums(n);
-
-    for (int i = 0; i < n; i++)
-    {
-        cin >> nums[i];
-    }
-
-    int threshold;
-    cin >> threshold;
-
-    cout << smallestDivisor(nums, threshold);
-
-    return 0;
+    printName(n);
 }
