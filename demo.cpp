ce4884766382928d731dbe5bf1a89e098d843cf0
Durgesh Shekhawat <durgeshshekhawat123456@gmail.com>
2025-08-13 13:00:04 +0530

    Capacity To Ship Packages Within D Days : Optimal Approach

--- demo.cpp
+++ demo.cpp
@@ -1,21 +1,77 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-void printName(int n)
+int countDays(vector<int> &weights, int capacity)
 {
+    int days = 1;
+    int load = 0;
 
-    if (n < 1)
+    for (int i = 0; i < weights.size(); i++)
     {
-        return;
+        if (load + weights[i] > capacity)
+        {
+            days += 1;
+            load = weights[i];
+        }
+        else
+        {
+            load += weights[i];
+        }
     }
-    cout << n << endl;
-    printName(n - 1);
-};
+
+    return days;
+}
+
+int shipWithinDays(vector<int> &weights, int days)
+{
+    int n = weights.size();
+
+    int maxxWeight = *max_element(weights.begin(), weights.end());
+
+    int summ = 0;
+    for (int i = 0; i < n; i++)
+    {
+        summ += weights[i];
+    }
+
+    int low = maxxWeight;
+    int high = summ;
+
+    while (low <= high)
+    {
+        int mid = low + (high - low) / 2;
+
+        int requiredDays = countDays(weights, mid);
+
+        if (requiredDays <= days)
+        {
+            high = mid - 1;
+        }
+        else
+        {
+            low = mid + 1;
+        }
+    }
+
+    return low;
+}
 
 int main()
 {
     int n;
     cin >> n;
 
-    printName(n);
+    vector<int> weights(n);
+
+    for (int i = 0; i < n; i++)
+    {
+        cin >> weights[i];
+    }
+
+    int days;
+    cin >> days;
+
+    cout << shipWithinDays(weights, days);
+
+    return 0;
 }
