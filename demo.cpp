dc199da124a2b8ad3b814d6588ad2eb3dd417a7c
Durgesh Shekhawat <durgeshshekhawat123456@gmail.com>
2025-08-15 11:10:07 +0530

    Find kth positive integer : brute froce approach

--- demo.cpp
+++ demo.cpp
@@ -1,46 +1,17 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int countDays(vector<int> &weights, int capacity)
+int findKthPositive(vector<int> &nums, int k)
 {
-    int days = 1;
-    int load = 0;
-
-    for (int i = 0; i < weights.size(); i++)
+    for (int i = 0; i < nums.size(); i++)
     {
-        if (load + weights[i] > capacity)
+        if (nums[i] <= k)
         {
-            days += 1;
-            load = weights[i];
+            k++;
         }
         else
         {
-            load += weights[i];
-        }
-    }
-
-    return days;
-}
-
-int shipWithinDays(vector<int> &weights, int days)
-{
-    int n = weights.size();
-
-    int maxxWeight = *max_element(weights.begin(), weights.end());
-
-    int summ = 0;
-    for (int i = 0; i < n; i++)
-    {
-        summ += weights[i];
-    }
-
-    for (int i = maxxWeight; i <= summ; i++)
-    {
-        int requiredDays = countDays(weights, i);
-
-        if (requiredDays <= days)
-        {
-            return i;
+            return k;
         }
     }
 
@@ -52,17 +23,17 @@ int main()
     int n;
     cin >> n;
 
-    vector<int> weights(n);
+    vector<int> nums(n);
 
     for (int i = 0; i < n; i++)
     {
-        cin >> weights[i];
+        cin >> nums[i];
     }
 
-    int days;
-    cin >> days;
+    int k;
+    cin >> k;
 
-    cout << shipWithinDays(weights, days);
+    cout << findKthPositive(nums, k);
 
     return 0;
 }
