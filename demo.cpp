c8950dcbdc9bb35ecd51e3d8f1bc7af2001098a7
Durgesh Shekhawat <durgeshshekhawat123456@gmail.com>
2025-08-28 15:54:58 +0530

    Reverse words in strings

--- demo.cpp
+++ demo.cpp
@@ -1,77 +1,56 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-double medianSortedArrays(vector<int> &nums1, vector<int> &nums2)
-{
-    int n1 = nums1.size();
-    int n2 = nums2.size();
-
-    int i = 0;
-    int j = 0;
-
-    vector<int> arr;
-
-    while (i < n1 && j < n2)
-    {
-        if (nums1[i] <= nums2[j])
-        {
-            arr.push_back(nums1[i]);
-            i++;
-        }
-        else
-        {
-            arr.push_back(nums2[j]);
-            j++;
-        }
-    }
-
-    while (i < n1)
-    {
-        arr.push_back(nums1[i]);
-        i++;
-    }
-
-    while (j < n2)
-    {
-        arr.push_back(nums2[j]);
-        j++;
-    }
-
-    int n = arr.size();
-
-    if (n % 2 == 0)
-    {
-        return (arr[n / 2] + arr[n / 2 - 1]) / 2.0;
-    }
-    else
-    {
-        return arr[n / 2];
-    }
-}
-
 int main()
 {
-    int n1;
-    cin >> n1;
+    string str;
+    getline(cin, str);
 
-    vector<int> nums1(n1);
+    int low = 0;
+    int high = str.length() - 1;
 
-    for (int i = 0; i < n1; i++)
+    while (low <= str.length() / 2)
     {
-        cin >> nums1[i];
+        char temp = str[low];
+        str[low] = str[high];
+        str[high] = temp;
+
+        low++;
+        high--;
     }
 
-    int n2;
-    cin >> n2;
+    string ans = "";
 
-    vector<int> nums2(n2);
-
-    for (int i = 0; i < n2; i++)
+    for (int i = 0; i < str.size(); i++)
     {
-        cin >> nums2[i];
+        string word = "";
+        while (i < str.length() && str[i] != ' ')
+        {
+            word += str[i];
+            i++;
+        }
+
+        int j = 0;
+        int k = word.length() - 1;
+
+        while (j < k)
+        {
+            char temp = word[j];
+            word[j] = word[k];
+            word[k] = temp;
+
+            j++;
+            k--;
+        }
+
+        if (word.length() > 0)
+        {
+            ans += " " + word;
+        }
     }
 
-    cout << medianSortedArrays(nums1, nums2);
+    string newString = ans.substr(1);
+    cout << newString;
 
     return 0;
 }
