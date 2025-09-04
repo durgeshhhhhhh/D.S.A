1c63930074b3b42ddb2123a8888d995aae072b44
Durgesh Shekhawat <durgeshshekhawat123456@gmail.com>
2025-09-04 11:10:20 +0530

    Pointers

--- demo.cpp
+++ demo.cpp
@@ -1,54 +1,13 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int myAtoi(string s)
-{
-    int n = s.length();
-
-    int i = 0;
-
-    if (i < n && s[i] == ' ')
-    {
-        i++;
-    }
-
-    if (i == n)
-        return 0;
-
-    int sign = 1;
-    if (s[i] == '-')
-    {
-        sign = -1;
-        i++;
-    }
-    else if (s[i] == '+')
-    {
-        i++;
-    }
-
-    long long result = 0;
-    while (i < n && isdigit(s[i]))
-    {
-        int digit = s[i] - '0';
-
-        if (result > (INT_MAX - digit) / 10)
-        {
-            return sign == 1 ? INT_MAX : INT_MIN;
-        }
-
-        result = result * 10 + digit;
-        i++;
-    }
-
-    return sign * result;
-}
-
 int main()
 {
-    string s;
-    cin >> s;
+    int n = 10;
 
-    cout << myAtoi(s);
+    cout << &n << endl;
 
-    return 0;
+    int *ptr = &n;
+
+    cout << ptr;
 }
