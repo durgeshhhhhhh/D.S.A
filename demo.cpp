74dae5f4f92b50eb4b489334911e0284d2fce147
Durgesh Shekhawat <durgeshshekhawat123456@gmail.com>
2025-09-10 12:14:35 +0530

    which constructor or destructor called first

--- demo.cpp
+++ demo.cpp
@@ -7,35 +7,48 @@ public:
     string name;
     int age;
 
-    // Person(string name, int age)
-    // {
-    //     this->name = name;
-    //     this->age = age;
-    // }
+    Person()
+    {
+        cout << "I am Parent Constructor" << endl;
+    }
 
-    Person() {}
+    ~Person()
+    {
+        cout << "I am Parent Destructor" << endl;
+    }
 };
 
 class Student : public Person
 {
 public:
-    int rollNo;
+    int roll;
+
+    ~Student()
+    {
+        cout << "I am child destructor" << endl;
+    }
+
+    Student()
+    {
+        cout << "I am child Constructor" << endl;
+    }
 
     void getInfo()
     {
-        cout << "name : " << name << endl;
-        cout << "age : " << age << endl;
-        cout << "roll no: : " << rollNo << endl;
+        cout << "name: " << name << endl;
+        cout << "age: " << age << endl;
+        cout << "roll: " << roll << endl;
     }
 };
 
 int main()
 {
+
     Student s1;
 
-    s1.name = "Yogesh";
-    s1.age = 21;
-    s1.rollNo = 1234;
+    s1.name = "yogesh";
+    s1.age = 12;
+    s1.roll = 12390;
 
     s1.getInfo();
 
