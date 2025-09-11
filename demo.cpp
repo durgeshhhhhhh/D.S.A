33616bd93f8d81057298756abf4ea06520ab2b80
Durgesh Shekhawat <durgeshshekhawat123456@gmail.com>
2025-09-11 09:25:27 +0530

    compile time polymorphism : example -> Constructor Overloading

--- demo.cpp
+++ demo.cpp
@@ -1,51 +1,26 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-class Person
+class Student
 {
 public:
     string name;
-    int age;
-};
 
-class Teacher : public Person
-{
-public:
-    string subject;
-};
+    Student()
+    {
+        cout << "non parameterized\n";
+    }
 
-class Student : public Person
-{
-public:
-    int roll;
+    Student(string name)
+    {
+        this->name = name;
+        cout << name;
+    }
 };
 
 int main()
 {
-
-    Teacher t1;
-
-    t1.name = "Saumya shekhawat";
-    t1.age = 21;
-    t1.subject = "Maths";
-
-    cout << t1.name << endl;
-    cout << t1.age << endl;
-    cout << t1.subject << endl;
-
-    cout << endl
-         << endl
-         << endl;
-
-    Student s1;
-
-    s1.name = "Durgesh shekhawat";
-    s1.age = 18;
-    s1.roll = 36;
-
-    cout << s1.name << endl;
-    cout << s1.age << endl;
-    cout << s1.roll << endl;
+    Student s1("Durgesh");
 
     return 0;
 }
