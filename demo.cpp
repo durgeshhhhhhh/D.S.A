d9266cf40b8260bb0ccf9c859557c0aaf5ea2494
Durgesh Shekhawat <durgeshshekhawat123456@gmail.com>
2025-10-29 21:41:42 +0530

    Largest Rectangle in Histogram

--- demo.cpp
+++ demo.cpp
@@ -6,39 +6,55 @@ int main()
     int n;
     cin >> n;
 
-    vector<int> nums(n);
+    vector<int> heights(n);
 
     for (int i = 0; i < n; i++)
     {
-        cin >> nums[i];
+        cin >> heights[i];
     }
 
-    vector<int> ans(n, 0);
+    vector<int> left(n, 0);
+    vector<int> right(n, 0);
     stack<int> st;
 
     for (int i = 0; i < n; i++)
     {
-        while (st.size() > 0 && st.top() >= nums[i])
+        while (!st.empty() && heights[st.top()] >= heights[i])
         {
             st.pop();
         }
 
-        if (st.empty())
-        {
-            ans[i] = -1;
-        }
-        else
-        {
-            ans[i] = st.top();
-        }
+        left[i] = st.empty() ? -1 : st.top();
 
-        st.push(nums[i]);
+        st.push(i);
     }
 
+    while (!st.empty())
+    {
+        st.pop();
+    }
+
+    for (int i = n - 1; i >= 0; i--)
+    {
+        while (!st.empty() && heights[st.top()] >= heights[i])
+        {
+            st.pop();
+        }
+
+        right[i] = st.empty() ? n : st.top();
+
+        st.push(i);
+    }
+
+    int ans = 0;
     for (int i = 0; i < n; i++)
     {
-        cout << ans[i] << " ";
+        int height = heights[i];
+        int width = right[i] - left[i] - 1;
+        ans = max(ans, height * width);
     }
+
+    cout << ans;
 
     return 0;
 }
